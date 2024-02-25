770974fb3b0c9882db8f8029f0c2b3dfeea1d118
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-02-25 21:31:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+    let bold=document.querySelectorAll('strong');
+	bold.forEach(boldwords=>{
+		boldwords.style.color='green'
+	})
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+ let bold=document.querySelectorAll('strong');
+	bold.forEach(boldwords=>{
+		boldwords.style.color='black'
+	})
     
 }
